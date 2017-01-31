13f2f3e72469d16aef71d2aa17ce672a2b9f860a
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-01-31 03:01:22 +0300

    added config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$_CONFIG['work_dir'] = '/usr/local/guard_system';
+$_CONFIG['debug_level'] = array(LOG_ERR, LOG_WARNING, LOG_NOTICE);
+$_CONFIG['database_server'] = array("host" => '127.0.0.1',
+                                    "port" => '3306',
+                                    "user" => 'root',
+                                    "pass" => '13941',
+                                    "database" => 'guard_system');
