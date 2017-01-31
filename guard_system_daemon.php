2cb5c1e3dea5709597b4a9fce458cf621035465b
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-01-31 03:00:09 +0300

    added exceptions, move connect DB, modem tests

--- guard_system_daemon.php
+++ guard_system_daemon.php
@@ -1,12 +1,36 @@
 #!/usr/bin/php
 <?php
+require_once("config.php");
+require_once("os.php");
+require_once("database.php");
+require_once("modem3g.php");
 require_once("guard_system.php");
+
+$utility_name = 'guard_system';
+
+function error_exception($exception)
+{
+    echo 'Error: ' . $exception->getMessage() . "\n";
+    exit;
+}
 
 
 function main()
 {
-    $gs = new Guard_system;
-    $gs->do();
+    global $_CONFIG;
+    set_exception_handler('error_exception');
+
+    $db = new Database;
+    $rc = $db->connect($_CONFIG['database_server']);
+    if ($rc)
+        throw new Exception("can't connect to database");
+
+  //  $modem = new Modem3G;
+    //$modem->send_sms("5051024", "бла бла тест");
+   // $gs = new Guard_system($db);
+   // $gs->do();
+
+
 }
 
 return main();
