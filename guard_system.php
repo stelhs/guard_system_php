a12048b6dfadc57e5078a6d0a835665a3b4c7838
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-01-31 03:00:55 +0300

    Mysql API was changed into guard_system.php

--- guard_system.php
+++ guard_system.php
@@ -1,30 +1,27 @@
 <?php
 require_once("io_module.php");
-require_once("base_sql.php");
-
 
 
 class Guard_system {
     private $sensors;
     private $io_module;
+    private $db;
+
+    function __construct($database)
+    {
+        $db = $database;
+    }
 
     function open()
     {
-        $db_settings = array("host" => '127.0.0.1  ',
-                             "user" => 'root',
-                             "pass" => '13941',
-                             "database" => 'automation');
+        global $_CONFIG;
     
-        $rc = db_init($db_settings);
+        $rc = db_init($_CONFIG['database_server']);
         if ($rc)
             return -ECONNFAIL;
 
-        $rows = db_query("SELECT * FROM sensors");
-        $this->sensors = array();
-        foreach ($rows as $row)
-            $this->sensors[$row['id']] = $row;
-
-        $this->io_module = new Io_module;
+        $this->sensors = $this->db->query_list("SELECT * FROM sensors");
+        $this->io_module = new Io_module($this->db);
         $this->io_module->open();
         return 0;
     }
@@ -35,87 +32,78 @@ class Guard_system {
         $this->io_module->close();
     }
 
-    function do()
+    function main_cycle()
     {
-        for (;;) {
-            $resp = $this->io_module->wait_new_data(1);
-            f (!is_array($resp))
-                continue;
-
+        $resp = $this->io_module->wait_new_data(1);
+        if (is_array($resp)) {
             switch ($resp['type']) {
             case 'io':
                 $this->push_io_msg($resp['msg']);
                 break;
-            }
+            
             case 'control':
                 $this->push_control_msg($resp['msg']);
                 break;
             }
+        }
 
-            $list = get_list_new_sms();
-            if (is_array($list)) {
-                //TODO: analysis incommong SMS 
-            }
+        $list = get_list_new_sms();
+        if (is_array($list)) {
+            //TODO: analysis incommong SMS 
         }
     }
 
-    private function push_io_msg($msg)
+    function push_io_msg($msg)
     {
         switch ($msg['si']) {
         case 'AIP': /* Action on Input Port */
             $port = $msg[0];
             $state = $msg[1];
-            db_insert('io_input_actions', array('port' => $port,
-                                                'state' => $state));
+
+            $this->db->insert('io_input_actions', array('port' => $port,
+                                                        'state' => $state));
 
             $this->update_sensor($port, $state);
             break;
 
         case 'SOP': /* State Output Port */
-            db_insert('io_output_actions', array('port' => $msg[0],
-                                                'state' => $msg[1]));
+            $this->db->insert('io_output_actions', array('port' => $msg[0],
+                                                         'state' => $msg[1]));
             break;
         }
     }
 
-    private function push_control_msg($msg)
+    function push_control_msg($msg)
     {
 
     }
 
-    private function get_guard_state()
+    function get_guard_state()
     {
-        $rows = db_query("SELECT * FROM guard_actions ORDER by created DESC LIMIT 1");
-        $result = db_query($query);
+        $data = $this->db->query("SELECT * FROM guard_actions ORDER by created DESC LIMIT 1");
+        if (!$data)
+            return $data;
         
-        if ($result == FALSE)
-            return -ESQL;
-        else 
-            return $result[0]['state'];
+        return $data['state'];
     }
 
-    private function get_sensor_locking_mode($sensor_id)
+    function get_sensor_locking_mode($sensor_id)
     {
-        $rows = db_query("SELECT * FROM blocking_sensors " .
-                         "WHERE sense_id = " . $sensor_id . " " .
-                         "ORDER by created DESC LIMIT 1");
-        $result = db_query($query);
-        
-        if ($result == FALSE)
-            return -ESQL;
-        else 
-            return $result[0]['mode'];
+        $data = $this->db->query("SELECT * FROM blocking_sensors " .
+                                 "WHERE sense_id = " . $sensor_id . " " .
+                                 "ORDER by created DESC LIMIT 1");
+        return $data['mode'];
     }
 
-    private function update_sensor($port, $state)
+    function update_sensor($port, $state)
     {
         $sensor = $this->sensors[$port];
         $sensor_state = ($state == $sensor['normal_state'] ? 'normal' : 'action');
         $guard_state = $this->get_guard_state();
-        $action_id = db_insert('sensor_actions', 
-                               array('sense_id' => $sensor['id'],
-                                     'state' => $sensor_state,
-                                     'guard_state' => $guard_state));
+        $action_id = $this->db->insert('sensor_actions', 
+                                       array('sense_id' => $sensor['id'],
+                                             'state' => $sensor_state,
+                                             'guard_state' => $guard_state));
 
         $sense_locking_mode = get_sensor_locking_mode($sensor['id']);
         if ($sense_locking_mode == 'lock')
@@ -125,23 +113,23 @@ class Guard_system {
 //            $this->do_alarm($action_id);
     }
 
-    private function do_alarm($action_id)
+    function do_alarm($action_id)
     {
         save_camera_images($action_id);
-        this->io_module->relay_set_state($port, $state);
+        $this->io_module->relay_set_state($port, $state);
     }
 
-    private function save_camera_images($action_id)
+    function save_camera_images($action_id)
     {
 
     }
 
-    private function send_sms($text)
+    function send_sms($text)
     {
 
     }
 
-    private function get_list_new_sms()
+    function get_list_new_sms()
     {
 
     }
